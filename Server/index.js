6a3322a6e4fa491b4034ce5f0c7a5df51a7144cd
kshitij2201 <kshitijmeshram31@gmail.com>
2024-12-22 11:15:20 +0530

    updation

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import cors from "cors";
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
@@ -28,5 +31,5 @@ app.get("/", (req, res) => {
 });
 
 
-module.exports = app;
+
 // updated with the code
